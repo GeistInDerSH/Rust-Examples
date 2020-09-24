47982b80adf03dfbc76825aaffe1e8faef41c845
GeistInDerSH <geistindersh@gmail.com>
2020-09-24 14:07:40 -0700

    Added a function to print out each string in an array to the terminal as the selection options. Added a selection window where the user can scroll up and down to select the program they with to run.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate ncurses;
+use ncurses::*;
+
 mod bottles99;
 mod armstrong;
 mod magic8ball;
@@ -5,50 +8,92 @@ mod pythagoreantriples;
 mod rock_paper_scissors;
 mod multiplication_tables;
 
-fn list_modules()
+fn print_options(options: [&'static str; 6], pos: i8)
 {
-    println!("Available Options:");
-    println!("0) 99 Bottles of beer");
-    println!("1) Armstrong Number");
-    println!("2) Magic 8 Ball");
-    println!("3) Pythagorean Triples");
-    println!("4) Rock Paper Scissors");
-    println!("5) Multiplication Tables");
+    let mut i = 0;
+    addstr("Available Options:\n");
+    for s in options.iter()
+    {
+        let marker = if pos == i { ">" } else { "" };
+        addstr(&format!("{0: <3} {1:}\n", marker, s));
+        i += 1;
+    }
+}
+
+fn list_modules() -> i8
+{
+
+    let options = [
+        "99 Bottles of beer",
+        "Armstrong Number",
+        "Magic 8 Ball",
+        "Pythagorean Triples",
+        "Rock Paper Scissors",
+        "Multiplication Tables"
+    ];
+
+    initscr();
+
+    let mut pos: i8 = 0;
+    print_options(options, pos);
+
+    loop
+    {
+        let c = std::char::from_u32(getch() as u32).unwrap();
+        clear();
+
+        match c
+        {
+            'w' => { pos = if pos == 0 { options.len() as i8  -1 } else { pos - 1 } },
+            's' => { pos = (pos + 1) % options.len() as i8; },
+            '\n' => { break; },
+            _ => {},
+        }
+
+        addstr("Available Options:\n");
+
+        let mut i = 0;
+        for s in options.iter()
+        {
+            let marker = if pos == i { ">" } else { "" };
+            addstr(&format!("{0: <3} {1:}\n", marker, s));
+            i += 1
+        }
+
+        refresh();
+    }
+    endwin();
+
+    return pos;
 }
 
 fn main()
 {
-    list_modules();
-    println!("Enter your selection: ");
-    let mut user_input = String::new();
-    std::io::stdin().read_line(&mut user_input).unwrap();
+    let choice = list_modules();
 
-    user_input.truncate(user_input.len() - 1);
-
-    match user_input.as_str() {
-        "0" => {
+    match choice {
+        0 => {
             bottles99::print_bottles(99);
         },
-        "1" => {
-            user_input = "".to_string();
+        1 => {
+            let mut user_input = String::new();
             std::io::stdin().read_line(&mut user_input).unwrap();
             user_input.truncate(user_input.len() - 1);
-            println!("{}", user_input);
             armstrong::print_is_armstrong(user_input.parse().unwrap());
         },
-        "2" => {
+        2 => {
             magic8ball::magic_8_ball();
         },
-        "3" => {
+        3 => {
             pythagoreantriples::pythagorean_triple();
         },
-        "4" => {
+        4 => {
             rock_paper_scissors::start();
         },
-        "5" => {
+        5 => {
             multiplication_tables::start();
         },
 
-        _ => {println!("{}", user_input.as_str());},
+        _ => {},
     }
 }
