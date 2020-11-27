3c1a49e4477d238ee42b53e640db2fc44ede874b
GeistInDerSH <geistindersh@gmail.com>
2020-11-26 21:27:39 -0700

    Moved the check for an armstrong number to its own function. Added test cases

--- src/armstrong/mod.rs
+++ src/armstrong/mod.rs
@@ -1,13 +1,14 @@
-/// print_is_armstrong prints if the given number is an armstrong number.
+/// Check to see if the given number is an armstrong number.
+///
 /// An armstrong number is one where the sum of the numbers to the power of the
 /// length of the number is equal to the number.
-/// # Example
-/// ```rust
+///
+/// # Example:
+/// ```
 /// let foo: i64 = 153;
 /// assert_eq!(foo, 1*1*1 + 5*5*5 + 3*3*3);
 /// ```
-pub fn print_is_armstrong(n: i64)
-{
+fn is_armstrong(n: i64) -> bool {
     // Convert the number to a string so we can get each position of the number
     let n_str = n.to_string();
 
@@ -27,8 +28,60 @@ pub fn print_is_armstrong(n: i64)
         sum += val.pow(len as u32);
     }
 
-    // Print that if it is an armstrong number iff
-    // the sum is equal to the number given.
-    let result = if sum == n { "" } else { "not " };
+    sum == n
+}
+
+/// Prints if the given number is an armstrong number.
+pub fn print_is_armstrong(n: i64)
+{
+    let result = if is_armstrong(n) { "" } else { "not " };
     println!("{} is {}an armstrong number!", n, result);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn is_armstrong_0() {
+        let armstrong_number: i64 = 0;
+        let result: bool = is_armstrong(armstrong_number);
+        assert_eq!(true, result);
+    }
+
+    #[test]
+    fn is_not_armstrong_120() {
+        let armstrong_number: i64 = 120;
+        let result: bool = is_armstrong(armstrong_number);
+        assert_eq!(false, result);
+    }
+
+    #[test]
+    fn is_armstrong_153() {
+        let armstrong_number: i64 = 153;
+        let result: bool = is_armstrong(armstrong_number);
+        assert_eq!(true, result);
+    }
+
+    #[test]
+    fn is_not_armstrong_241() {
+        let armstrong_number: i64 = 241;
+        let result: bool = is_armstrong(armstrong_number);
+        assert_eq!(false, result);
+    }
+
+    #[test]
+    fn is_armstrong_370() {
+        let armstrong_number: i64 = 370;
+        let result: bool = is_armstrong(armstrong_number);
+        assert_eq!(true, result);
+    }
+
+    #[test]
+    fn is_armstrong_1634() {
+        let armstrong_number: i64 = 1634;
+        let result: bool = is_armstrong(armstrong_number);
+        assert_eq!(true, result);
+    }
+
 }
