ab23655469afbb6320865c887c84c94a282bd769
GeistInDerSH <geistindersh@gmail.com>
2020-11-26 21:28:14 -0700

    Changed normal comments to docstring comments

--- src/magic8ball/mod.rs
+++ src/magic8ball/mod.rs
@@ -3,8 +3,8 @@ use std::time::Duration;
 use std::thread;
 use std::io::{self, Write};
 
-// magic_8_ball is the entrypoint for this module. It prints out the menu,
-// gets the users input and question, then generates a response.
+/// magic_8_ball is the entrypoint for this module. It prints out the menu,
+/// gets the users input and question, then generates a response.
 pub fn magic_8_ball()
 {
     loop {
@@ -25,8 +25,8 @@ pub fn magic_8_ball()
     }
 }
 
-// think prints that the magic 8 ball is thinking with a 1 second delay between each
-// period ( '.' ).
+/// think prints that the magic 8 ball is thinking with a 1 second delay between each
+/// period ( '.' ).
 fn think()
 {
     print!("Thinking");
@@ -39,13 +39,13 @@ fn think()
     println!("");
 }
 
-// print_menu prints out the menu for the user to respond to.
+/// print_menu prints out the menu for the user to respond to.
 fn print_menu()
 {
     println!("Options:\na: Ask a question\nq: quit");
 }
 
-// get_input reads a single line from stdin and removes the trailing newline.
+/// get_input reads a single line from stdin and removes the trailing newline.
 fn get_input() -> String
 {
     let mut user_input = String::new();
@@ -54,9 +54,9 @@ fn get_input() -> String
     return user_input;
 }
 
-// get_response generates a random response from the 20 options available inside a
-// magic 8 ball. The first 10 are affirmative, the next 5 are non-commital, and the
-// last 5 are negative.
+/// get_response generates a random response from the 20 options available inside a
+/// magic 8 ball. The first 10 are affirmative, the next 5 are non-commital, and the
+/// last 5 are negative.
 fn get_response() -> String
 {
     let response: [&str; 20] = [
